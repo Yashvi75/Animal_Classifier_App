8572a0b29eba99cf2f023484e6509efe08446ac5
Yashvi75 <yashvivyas.work@gmail.com>
2025-04-10 18:01:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ st.write("Upload an image of an animal and I will tell you which one it is!")
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
+    img = Image.open(uploaded_file)
+    st.image(img, caption="Uploaded Image", use_column_width=True)
     # 🧠 Preprocess and Predict
     img = img.resize((224, 224))
     img_array = image.img_to_array(img) / 255.0
